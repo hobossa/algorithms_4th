0d631670774fc4632212610f860f39d1026e2f4e
hobossa <hoboss@gmail.com>
2021-02-10 06:03:35 +0800

    week 3 assignment score 82

--- algs4/src/main/java/FastCollinearPoints.java
+++ algs4/src/main/java/FastCollinearPoints.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,31 +26,29 @@ public class FastCollinearPoints {
         // finds segments containing 4 or more points
         Point[] auxiliaryPoints = new Point[pointsEx.length];
         for (int i = 0; i < pointsEx.length; i++) {
-            // copy point [i+1..points.length) to auxiliaryPoints
-            int len = pointsEx.length - i - 1;
-            if (len < 3) {
-                break;
-            }
-            for (int j = 0; j < len; j++) {
-                auxiliaryPoints[j] = pointsEx[i + 1 + j];
-            }
+            // copy point to auxiliaryPoints
+            System.arraycopy(pointsEx, 0, auxiliaryPoints, 0, auxiliaryPoints.length);
             // Sort
-            Arrays.sort(auxiliaryPoints, 0, len, pointsEx[i].slopeOrder());
+            Arrays.sort(auxiliaryPoints, pointsEx[i].slopeOrder());
+
             // find elements with same slopTo point[i]
             int m = 0;
             int n = 0;
-            for (int j = 1; j < len; j++) {
+            for (int j = 1; j < auxiliaryPoints.length; j++) {
                 if (pointsEx[i].slopeTo(auxiliaryPoints[j]) == pointsEx[i].slopeTo(auxiliaryPoints[m])) {
-                    n = i;
+                    n = j;
                 } else {
-                    m = i;
+                    // auxiliaryPoints[m..n] are collinear
+                    if (n - m >= 2) {
+                        // Arrays.sort sorts the array in place. So auxiliaryPoints[m] is the smallest
+                        // and the auxiliaryPoints[n] is the biggest. Add LineSegment in to segments
+                        // only if pointsEx[i] is the start point (smaller than auxiliaryPoint[m])
+                        if (pointsEx[i].compareTo(auxiliaryPoints[m]) < 0) {
+                            segments.add(new LineSegment(pointsEx[i], auxiliaryPoints[n]));
+                        }
+                    }
+                    m = j;
                 }
-            }
-            // auxiliaryPoints[m..n] are collinear
-            if (n - m >= 2) {
-                // Arrays.sort sorts the array in place. So we point[i] is the smallest
-                // and the auxiliaryPoints[r] is the biggest.
-                segments.add(new LineSegment(pointsEx[i], auxiliaryPoints[n]));
             }
         }
 
@@ -75,4 +74,31 @@ public class FastCollinearPoints {
             throw new IllegalArgumentException("null value");
         }
     }
+
+    ///// test
+//    public static void main(String[] args) {
+//        //10000,  0
+//        //0,      10000
+//        //3000,   7000
+//        //7000,   3000
+//        //20000,  21000
+//        //3000,   4000
+//        //14000,  15000
+//        //6000,   7000
+//        Point[] points = new Point[8];
+//        points[0] = new Point(10000, 0);
+//        points[1] = new Point(0, 10000);
+//        points[2] = new Point(3000, 7000);
+//        points[3] = new Point(7000, 3000);
+//        points[4] = new Point(20000, 21000);
+//        points[5] = new Point(3000, 4000);
+//        points[6] = new Point(14000, 15000);
+//        points[7] = new Point(6000, 7000);
+//        FastCollinearPoints fcp = new FastCollinearPoints(points);
+//        StdOut.println(fcp.numberOfSegments());
+//        for (LineSegment line : fcp.segments()) {
+//            StdOut.println(line);
+//        }
+//    }
+
 }
