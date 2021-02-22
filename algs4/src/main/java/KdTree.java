3ce7aef3ad1a29bb3d87e53f81b6f15556fa9527
hobossa <hoboss@gmail.com>
2021-02-23 07:00:10 +0800

    KdTree nearest ing...

--- algs4/src/main/java/KdTree.java
+++ algs4/src/main/java/KdTree.java
@@ -1,45 +1,210 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
-// Implementation requirements.  You must use either edu.princeton.cs.algs4.SET or java.util.TreeSet;
-// do not implement your own red–black BST.
+import java.util.LinkedList;
+import java.util.List;
+
+// simple implementation, do not considerate tree balance
 public class KdTree {
+    // ------ nested class Node --------
+    private static class Node {
+        Node left = null;
+        Node right = null;
+        Node parent;
+        private final Point2D p;
+        private final int level; // 0 for vertical splits, 1 for horizontal splits
+
+        public Node(Point2D p) {
+            this.p = p;
+            this.level = 0;
+            this.parent = null;
+        }
+
+        public Node(Point2D p, Node parent) {
+            this.parent = parent;
+            this.p = p;
+            this.level = (parent.level + 1) % 2;
+        }
+    } // ------ end of nested class Node --------
+
+    private Node root = null;
+    private int size = 0;
+
     // construct an empty set of points
     public KdTree() {
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return 0 == size();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        if (null == root) {
+            root = new Node(p);
+            size++;
+            return;
+        }
 
+        Node cur = root;
+        Node parent = root;
+        while (cur != null) {
+            parent = cur;
+            if (p.equals(cur.p)) {
+                return;
+            }
+            if (0 == cur.level) {
+                if (p.x() < cur.p.x()) {
+                    cur = cur.left;
+                } else {
+                    cur = cur.right;
+                }
+            } else {
+                // 1 == node.level
+                if (p.y() < cur.p.y()) {
+                    cur = cur.left;
+                } else {
+                    cur = cur.right;
+                }
+            }
+        }
+        Node node = new Node(p, parent);
+        if (0 == parent.level) {
+            if (p.x() < parent.p.x()) {
+                parent.left = node;
+            } else {
+                parent.right = node;
+            }
+        } else {
+            // 1 == node.level
+            if (p.y() < parent.p.y()) {
+                parent.left = node;
+            } else {
+                parent.right = node;
+            }
+        }
+        size++;
     }
+
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        Node cur = root;
+        while (cur != null) {
+            if (p.equals(cur.p)) {
+                return true;
+            }
+            if (0 == cur.level) {
+                if (p.x() < cur.p.x()) {
+                    cur = cur.left;
+                } else {
+                    cur = cur.right;
+                }
+            } else {
+                // 1 == node.level
+                if (p.y() < cur.p.y()) {
+                    cur = cur.left;
+                } else {
+                    cur = cur.right;
+                }
+            }
+        }
         return false;
     }
 
     // draw all points to standard draw
     public void draw() {
-
+        // travel the tree
+        // draw a black point
+        // draw a red horizontal line (0 == level) or a blue vertical line (1 == level)
+        // the line starts at the edge and end at the parent's line.
+        // the first line (for root node) start at and end at edges.
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (null == rect) {
+            throw new IllegalArgumentException();
+        }
+        List<Point2D> points = new LinkedList<>();
+        range(rect, points, root);
+        return points;
+    }
+
+    private void range(RectHV rect, List<Point2D> points, Node node) {
+        if (null == node) {
+            return;
+        }
+        if (rect.contains(node.p)) {
+            points.add(node.p);
+        }
+        if (0 == node.level) {
+            if (rect.xmin() < node.p.x()) {
+                range(rect, points, node.left);
+            }
+            if (rect.xmax() >= node.p.x()) {
+                range(rect, points, node.right);
+            }
+        } else {
+            // 1 == node.level
+            if (rect.ymin() < node.p.y()) {
+                range(rect, points, node.left);
+            }
+            if (rect.ymax() >= node.p.y()) {
+                range(rect, points, node.right);
+            }
+        }
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        Node near = root;
+        double minDistance = Double.MAX_VALUE;
+
+        return null;
+    }
+
+    private Node nearest(Point2D p, Node node, double distance) {
+        if (node == null) {
+            return null;
+        }
+        double d = p.distanceTo(node.p);
+        if (d < distance) {
+            distance = d;
+        }
+        Node n1 = null, n2 = null;
+        if (0 == node.level) {
+            if (p.x() < node.p.x()) {
+                n1 = nearest(p, node.left, distance);
+                if (distance >= node.p.x() - p.x()) {
+                    // we still need to search right
+                    n2 = nearest(p, node.right, distance);
+                }
+            } else {
+                n1 = nearest(p, node.right, distance);
+                if (distance > p.x()-node.p.x()) {
+                    // we Still need to search Left
+                    n2 = nearest(p, node.left, distance);
+                }
+            }
+        } else {
+            // 1 == node.level
+        }
         return null;
     }
 
--- algs4/src/main/java/PointSET.java
+++ algs4/src/main/java/PointSET.java
@@ -1,49 +1,85 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
-// Implementation requirements.  You must use either edu.princeton.cs.algs4.SET or java.util.TreeSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+// Implementation requirements.
+// You must use either edu.princeton.cs.algs4.SET or java.util.TreeSet;
 // do not implement your own red–black BST.
 
 // Brute-force implementation.
 public class PointSET {
+    private final Set<Point2D> points = new TreeSet<>();
+
     // construct an empty set of points
     public PointSET() {
-
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-
+        for (Point2D p : points) {
+            p.draw();
+        }
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (null == rect) {
+            throw new IllegalArgumentException();
+        }
+        List<Point2D> listP = new LinkedList<>();
+        for (Point2D p : points) {
+            if (rect.contains(p)) {
+                listP.add(p);
+            }
+        }
+        return listP;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if (null == p) {
+            throw new IllegalArgumentException();
+        }
+        Point2D pNearest = null;
+        double minDistance = Double.MAX_VALUE;
+        for (Point2D point : points) {
+            double distance = point.distanceTo(p);
+            if (distance < minDistance) {
+                minDistance = distance;
+                pNearest = point;
+            }
+        }
+        return pNearest;
     }
 
     // unit testing of the methods (optional)
