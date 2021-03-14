3477c6e1c87edfd2218d508a5868ee3958390dc3
hobossa <hoboss@gmail.com>
2021-03-14 10:10:56 +0800

    Update SeamCarver.java
    96

--- algs4/src/main/java/SeamCarver.java
+++ algs4/src/main/java/SeamCarver.java
@@ -76,6 +76,10 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        if (x < 0 || x > width() - 1
+                || y < 0 || y > height() - 1) {
+            throw new IllegalArgumentException();
+        }
         int i = x + y * width();
         return energyMatrix[i];
     }
@@ -225,7 +229,7 @@ public class SeamCarver {
         int maxLine = posToRemove[posToRemove.length - 1] / width();
         for (int pos : posToRemove) {
             int nMove = pos + width();
-            while (nMove < (maxLine + 1) * width) {
+            while (nMove < (maxLine + 1) * width()) {
                 srcArray[nMove - width()] = srcArray[nMove];
                 nMove += width();
             }
@@ -242,7 +246,7 @@ public class SeamCarver {
         int maxLine = posToRemove[posToRemove.length - 1] / width();
         for (int pos : posToRemove) {
             int nMove = pos + width();
-            while (nMove < (maxLine + 1) * width) {
+            while (nMove < (maxLine + 1) * width()) {
                 srcArray[nMove - width()] = srcArray[nMove];
                 nMove += width();
             }
